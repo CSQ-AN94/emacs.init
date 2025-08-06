fc3cac77e1e712e1defb3e93c77643ca4e081986
CSQ-AN94 <csq15705100218@outlook.com>
2025-08-06 19:49:34 +0800

    存档，下面要动大东西了

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -1,18 +1,18 @@
- ;; (defun my/latex-hook ()
- ;;    (turn-on-cdlatex)
- ;;    (turn-on-reftex))
+ (defun my/latex-hook ()
+    (turn-on-cdlatex)
+    (turn-on-reftex))
 
- ;;  (use-package tex
- ;;    :ensure auctex
- ;;    ;; 若使用 straight, 注释前一行, 并取消下一行注释:
- ;;    ;; :straight auctex
- ;;    :custom
- ;;    (TeX-parse-self t) ; 自动解析 tex 文件
- ;;    (TeX-PDF-mode t)
- ;;    (TeX-DVI-via-PDFTeX t)
- ;;    :config
- ;;    (setq-default TeX-master t) ; 默认询问主文件
- ;;    (add-hook 'LaTeX-mode-hook 'my/latex-hook)) ; 加载LaTeX模式钩子
+  (use-package tex
+    :ensure auctex
+    ;; 若使用 straight, 注释前一行, 并取消下一行注释:
+    ;; :straight auctex
+    :custom
+    (TeX-parse-self t) ; 自动解析 tex 文件
+    (TeX-PDF-mode t)
+    (TeX-DVI-via-PDFTeX t)
+    :config
+    (setq-default TeX-master t) ; 默认询问主文件
+    (add-hook 'LaTeX-mode-hook 'my/latex-hook)) ; 加载LaTeX模式钩子
 
   (use-package cdlatex
     :after tex ; 保证 cdlatex 在 auctex 之后加载
